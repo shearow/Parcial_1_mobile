6452393f9840bad45a7b2025b700145a022cbb80
Nicolas Bietti <nicotds2015@hotmail.com>
2025-04-23 12:02:44 -0300

    Corrigiendo código

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-        println("Bienvenido al Club deportivo Los Ciberianos");
+        println("Bienvenido al Club deportivo Los Siberianos");
 };
